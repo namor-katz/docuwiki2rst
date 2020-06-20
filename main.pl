0c02192bd77a404ae6af7f939c1d2ce7e3215e3d
RomanK <namor925@gmail.com>
2020-06-20 21:53:22 +0700

    read and print file

--- main.pl
+++ main.pl
@@ -1,10 +1,27 @@
 #!/usr/bin/perl
 #
-# The traditional first program.
- 
 # Strict and warnings are recommended.
 use strict;
 use warnings;
- 
+#this module print $1, first arg
+use Data::Dumper qw(Dumper);
+
 # Print a message.
-print "Hello, World!\n";
+#print "Hello, World!\n";
+
+
+#print Dumper \@ARGV;
+
+my $filename = 'William_Blake.txt';
+my $i = 0;
+
+open(FH, '<', $filename) or die $!;
+
+while(<FH>){
+   print $_;
+   $i++;
+   if(($i % 5) == 0) {
+	   print "5 page!\n\n";}
+}
+
+close(FH);
